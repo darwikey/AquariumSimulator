986681bc3c3cf49bdf1387b24a6c3661bff05999
MIRAOUI Jibril <jmiraoui@religieuse.pedago.ipb.fr>
2015-05-12 17:36:28 +0200

    modif

--- Affichage/Parser.java
+++ Affichage/Parser.java
@@ -1,18 +1,17 @@
 import java.util.Scanner; 
 import java.util.LinkedList;
-
+import java.util.HashMap;
 
 	
 
 public class Parser { 
 
     Scanner sc;
-    private LinkedList <Juhnytg> fishPositions = null;
+    private HashMap <String,Point> fishPositionsHashMap;
 
     public Parser(){
 	sc = new Scanner(System.in);
-	fishPositions = new LinkedList<Juhnytg> ();
-
+	fishPositionsHashMap = new HashMap <String,Point> ();
     }
     
 
@@ -27,8 +26,7 @@ public class Parser {
 	    parts[j] = parts[j].replace("[","");
 	    parts[j] = parts[j].replace("]","");
 	}
-    	   	
-    	
+   	   	   	
 	for (int j=1 ; j < parts.length ; j++) {
 	 
 	    switch (j % 5){
@@ -55,25 +53,18 @@ public class Parser {
 		// fishposition contains the former position and the fishType of all previous fishses
 		// start a new fish at 0,0
 		// keep going with a previous fish with its former position
-		if (fishPositions.size() == 0){  
-		    //System.out.println("coucou");
-		    
+
+		if (fishPositionsHashMap.size() == 0){  		    
 		    fishes.add((j-1)/5, new Fish(fishType, size_x, size_y, coord_x, coord_y, speed, new Point(0,0)));
 		}
-
 		else {
-		    //System.out.println("PAS COUCOU");
 		    boolean found = false;
-		    //System.out.println("position : " + fishPositions.get(0).getPos());
-		    for (int i = 0 ; i < fishPositions.size() ; i++){			
-			if (fishType.equals(fishPositions.get(i).getFishType())){
-			    fishes.add((j-1)/5, new Fish(fishType, size_x, size_y, coord_x, coord_y, speed, fishPositions.get(i).getPos()));
-			    fishPositions.remove(i);
+		    if (fishPositionsHashMap.containsKey(fishType)){
+			fishes.add((j-1)/5, new Fish(fishType, size_x, size_y, coord_x, coord_y, speed, (Point)fishPositionsHashMap.get(fishType)));
+			    fishPositionsHashMap.remove(fishType);
 			    found = true;
-			    //break;
-			}
 		    }
-
+		    
 		    if (!found){
 			fishes.add((j-1)/5, new Fish(fishType, size_x, size_y, coord_x, coord_y, speed, new Point(0,0)));
 			System.out.println("not found");
@@ -81,7 +72,7 @@ public class Parser {
 		    }
 		    
 		}
-		
+					      		
 		break;
 	
 	    }
@@ -89,9 +80,11 @@ public class Parser {
 	return fishes;
 	
     }
-
-    public void fillPositionsList(LinkedList<Juhnytg> list){	
-    	fishPositions = list;
+    
+    public void fillPositionsHashMap(HashMap <String,Point> hm){	
+    	fishPositionsHashMap = hm;
     }
+
+
 
 }
--- Affichage/ReadAndSendConsoleOutput.java
+++ Affichage/ReadAndSendConsoleOutput.java
@@ -1,36 +1,31 @@
 import java.util.Scanner; 
 import java.io.PrintWriter;
 import java.util.LinkedList;
+
+
+
 public class ReadAndSendConsoleOutput implements Runnable{
 
     private Scanner sc;
     private PrintWriter out;
-    //private static LinkedList <String> listStarted = null;
-    //public static boolean isActivated = false;
     public static LinkedList <String> listCommands = null;
     
     
     public ReadAndSendConsoleOutput(PrintWriter pw){
     	sc = new Scanner(System.in);
-	//listStarted = new LinkedList <String> ();
-		listCommands = new LinkedList <String> ();
-		out = pw;
+	listCommands = new LinkedList <String> ();
+	out = pw;
     }
 
     public void run(){
     	String stock;
     	
     	while(true){
-    		stock = sc.nextLine();
-		//stock += "\n";
-		//System.out.println("stock : '" + stock+"'");
-    		listCommands.add(stock);
-		/*for (int i = 0 ; i < listCommands.size() ; i++){
-		    System.out.println("élément " + i + " : " + listCommands.get(i));
-		    }*/
+	    stock = sc.nextLine();
+	    listCommands.add(stock);
 	
-    		out.println(stock);
-    		out.flush();
+	    out.println(stock);
+	    out.flush();
     	}
     }
 
@@ -38,10 +33,6 @@ public class ReadAndSendConsoleOutput implements Runnable{
     	return listCommands;
     }
     
-    /* public static LinkedList<String> getListStarted (){
-    	return listStarted;
-	}*/
-
 
 }
 
